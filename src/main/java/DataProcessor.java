d340ff456f6ff0024613b51e36f44f6c39eea24b
Saleh Shakour <saleh.shakour2000@gmail.com>
2023-08-14 00:32:38 +0330

    Some changes

--- src/main/java/DataProcessor.java
+++ src/main/java/DataProcessor.java
@@ -7,9 +7,16 @@ import java.util.stream.Stream;
 
 public class DataProcessor {
     private String fileName;
+    private String stopWordsFileName;
+    private List<String> extractedSentences;
+    private List<String> stopWords;
 
-    public DataProcessor(String fileName) {
+    public DataProcessor(String fileName, String stopWordsFileName) {
         this.fileName = fileName;
+        this.stopWordsFileName = stopWordsFileName;
+
+        this.extractedSentences = this.extractSentences();
+        this.stopWords = this.extractStopWord();
     }
 
     public List<String> extractSentences() {
@@ -34,8 +41,19 @@ public class DataProcessor {
                 .toList();
     }
 
-    public int sentencesCounter(){
-        return extractSentences().size();
+    public List<String> extractStopWord() {
+        byte[] bytes = new byte[0];
+        try {
+            bytes = Files.readAllBytes(Paths.get(stopWordsFileName));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        String content = new String(bytes);
+        return Stream.of(content.split("\r?\n")).toList();
+    }
+
+    public int sentencesCounter() {
+        return this.extractedSentences.size();
     }
 
 }
